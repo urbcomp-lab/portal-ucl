727e443a23fcf776b78a3255b6bd4325417bf8e9
Murphyly <isysjoris@hotmail.com>
2025-09-09 21:00:12 -0300

    test: debug code

--- publications.php
+++ publications.php
@@ -6,12 +6,11 @@ use Seboettg\CiteProc\CiteProc;
 function groupByYear($data) {
     $grouped = array();
     foreach ($data as $item) {
-        $issued = (array)$item->issued;
-        $dateParts = $issued['date-parts'][0] ?? [null];
-        $year = $dateParts[0];
-
-        if (!$year) {
+        // "issued" pode não existir
+        if (!isset($item['issued']['date-parts'][0][0])) {
             $year = "Sem ano";
+        } else {
+            $year = $item['issued']['date-parts'][0][0];
         }
 
         if (!array_key_exists($year, $grouped)) {
@@ -22,50 +21,50 @@ function groupByYear($data) {
     return $grouped;
 }
 
+
 function printYearSection($year, $items, $citeProc) {
     echo "<div class=\"row justify-content-center\">
         <div class=\"col-lg-10\">
             <div class=\"section-title text-center pb-20\">
                 <h3 class=\"title\">$year</h3>
                 <div class=\"line m-auto\"></div>
-            </div> <!-- section title -->
+            </div>
         </div>
     </div>";
-    echo "--- [DEBUG] Verificando publicações do ano: $year ---\n";
+
+    echo "<pre>--- [DEBUG] Verificando publicações do ano: $year ---\n";
     foreach ($items as $item) {
-        $title = isset($item->title) ? $item->title : "TÍTULO NÃO ENCONTRADO";
+        $title = $item['title'] ?? "TÍTULO NÃO ENCONTRADO";
         echo "[DEBUG] Título: $title\n";
 
-        $authors_json = isset($item->author) ? json_encode($item->author) : "SEM AUTORES";
+        $authors_json = isset($item['author']) ? json_encode($item['author']) : "SEM AUTORES";
         echo "[DEBUG] Autores: $authors_json\n";
     }
-    echo "--- [DEBUG] Fim da verificação para o ano: $year. Tentando renderizar agora... ---\n";
+    echo "--- [DEBUG] Fim do ano $year ---\n</pre>";
 
     echo $citeProc->render($items, "bibliography");
 }
 
 try {
     $dataString = file_get_contents("publications.json");
-    if ($dataString === false)
-    {
+    if ($dataString === false) {
         throw new Exception("Error reading file publications.json!");
     }
+
     $style = StyleSheet::loadStyleSheet("harvard-cite-them-right");
     $citeProc = new CiteProc($style, "en-US");
-    $data = json_decode($dataString);
-    $data = json_decode($dataString);
+
+    $data = json_decode($dataString, true);
     if (json_last_error() !== JSON_ERROR_NONE) {
         throw new Exception("Erro ao decodificar o JSON: " . json_last_error_msg());
     }
+
     $grouped = groupByYear($data);
     krsort($grouped);
 
     foreach ($grouped as $year => $papers) {
         printYearSection($year, $papers, $citeProc);
     }
-
-
-    // $bibliography = $citeProc->render($data, "bibliography");
 
 } catch (Exception $e) {
     echo "Error loading papers: " . $e->getMessage() . "\n";
